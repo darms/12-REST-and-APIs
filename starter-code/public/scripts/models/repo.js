abe3a2066f3daac9862fe74240a11401dc47d3a2
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-18 17:12:12 -0800

    All TODO's are done

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,9 +6,20 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // TODO: DONE How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url:'https://api.github.com/user/repos?type=owner',
+      type: 'GET',
+      headers: {
+        Authorization: `token ${githubToken}`
+      }
+    }).then(data => {
+      repos.all = data;
+      console.log(repos.all);
+      callback();
+    }), (err =>console.error(err))
+    };
 
-  };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
